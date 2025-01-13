af2cfceb372e27801758e7254543fcef90e897a5
MariaMercedesLopez97 <mmercedeslopez97@gmail.com>
2025-01-13 14:44:44 -0300

    Actualizado

---
+++ config.py
@@ -0,0 +1,5 @@
+# Definir rutas de datos
+DATOS_DIR = "../datos"
+HOUSING_PATH = f"{DATOS_DIR}/housing.csv"
+ENERGIA_ESPANA_PATH = f"{DATOS_DIR}/energia_espana.csv"
+ENERGIA_BRASIL_PATH = f"{DATOS_DIR}/energia_brasil.csv"

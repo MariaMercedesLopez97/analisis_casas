e585cf02a3161b78e1643e6241a42bc7b105db1f
MariaMercedesLopez97 <167473751+MariaMercedesLopez97@users.noreply.github.com>
2025-01-28 17:25:29 -0300

    Delete config.py
    Eliminar

--- config.py
+++
@@ -1,5 +0,0 @@
-# Definir rutas de datos
-DATOS_DIR = "../datos"
-HOUSING_PATH = f"{DATOS_DIR}/housing.csv"
-ENERGIA_ESPANA_PATH = f"{DATOS_DIR}/energia_espana.csv"
-ENERGIA_BRASIL_PATH = f"{DATOS_DIR}/energia_brasil.csv"
